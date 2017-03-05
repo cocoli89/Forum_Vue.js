82440cc8a476d077d371f6c363b138819acd6d53
cocoli <hanstar1127@gmail.com>
2017-03-05 10:39:32 +0000

    Remove brightness luminosity

---
+++ models/thread.js
@@ -0,0 +1,42 @@
+let slug = require('slug')
+
+module.exports = (sequelize, DataTypes) => {
+	let Thread = sequelize.define('Thread', {
+		name: {
+			type: DataTypes.STRING,
+			set (val) {
+				this.setDataValue('name', val)
+				this.setDataValue('slug', slug(val).toLowerCase())
+			}
+		},
+		slug: DataTypes.STRING
+	}, {
+		classMethods: {
+			associate (models) {
+				Thread.belongsTo(models.User)
+				Thread.belongsTo(models.Category)
+				Thread.hasMany(models.Post)
+			},
+			includeOptions () {
+				let models = sequelize.models
+
+				return [
+					{ model: models.User, attributes: ['username', 'createdAt', 'color', 'updatedAt', 'id'] }, 
+					models.Category,
+					{ 
+						model: models.Post, 
+						include: [
+							{ model: models.User, attributes: ['username', 'createdAt', 'id', 'color'] }, 
+							{
+								model: models.Post, as: 'Replies', include:
+								[{ model: models.User, attributes: ['username', 'id', 'color'] }]	
+							}
+						]
+					}
+				]
+			}
+		}
+	})
+
+	return Thread
+}

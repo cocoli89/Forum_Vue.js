cf243a4b7554c752c8a049d6d53b464b6e931d08
cocoli <hanstar1127@gmail.com>
2017-03-19 18:28:13 +0000

    Change column type to text rather than string

---
+++ models/post.js
@@ -0,0 +1,53 @@
+let marked = require('marked')
+
+marked.setOptions({
+	highlight: function (code) {
+		return require('highlight.js').highlightAuto(code).value;
+	},
+	sanitize: true
+});
+
+
+module.exports = (sequelize, DataTypes) => {
+	let Post = sequelize.define('Post', {
+		content: {
+			type: DataTypes.TEXT,
+			set (val) {
+				this.setDataValue('content', marked(val))
+			}
+		},
+		replyingToUsername: DataTypes.STRING
+	}, {
+		instanceMethods: {
+			getReplyingTo () {
+				return Post.findByPrimary(this.replyId)
+			},
+			setReplyingTo (post) {
+				return post.getUser().then(user => {
+					return this.update({ replyingToUsername: user.username, replyId: post.id })
+				})
+			}
+		},
+		classMethods: {
+			associate (models) {
+				Post.belongsTo(models.User)
+				Post.belongsTo(models.Thread)
+				Post.hasMany(models.Post, { as: 'Replies', foreignKey: 'replyId' })
+			},
+			includeOptions () {
+				let models = sequelize.models
+
+				return [
+					{ model: models.User, attributes: ['username', 'createdAt', 'id', 'color'] }, 
+					{ model: models.Thread, include: [models.Category]} ,
+					{
+						model: models.Post, as: 'Replies', include:
+						[{ model: models.User, attributes: ['username', 'id', 'color'] }]	
+					}
+				]
+			}
+		}
+	})
+
+	return Post
+}

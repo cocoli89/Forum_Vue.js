6d5942b540c145299aaad1878e72169db3c14abe
cocoli <hanstar1127@gmail.com>
2017-09-25 23:48:58 +0100

    Fix bugs and wrong files moved during merge of two projects

---
+++ routes/report.js
@@ -0,0 +1,100 @@
+let express = require('express')
+let router = express.Router()
+
+let { User, Post, Report, Sequelize } = require('../models')
+const Errors = require('../lib/errors')
+
+router.all('*', (req, res, next) => {
+	if(req.session.loggedIn) {
+		next()
+	} else {
+		res.status(401)
+		res.json({
+			errors: [Errors.requestNotAuthorized]
+		})
+	}
+})
+router.post('/', async (req, res) => {
+	try {
+		let post = await Post.findById(req.body.postId)
+
+		if(!post) throw Report.InvalidPostId(req.body.postId)
+
+		let user = await User.findOne({
+			where: { username: req.session.username }
+		})
+
+		let report = await Report.create({ reason: req.body.reason })
+		report.setFlaggedByUser(user)
+		report.setPost(post)
+
+		res.json({
+			success: true
+		})
+	} catch (e) {
+		if(e instanceof Sequelize.ValidationError) {
+			res.status(400)
+			res.json(e)
+		} else {
+			console.log(e)
+
+			res.status(500)
+			res.json({
+				errors: [Errors.unknown]
+			})
+		}
+	}
+})
+
+router.all('*', (req, res, next) => {
+	if(req.session.admin) {
+		next()
+	} else {
+		res.status(401)
+		res.json({
+			errors: [Errors.requestNotAuthorized]
+		})
+	}
+})
+router.get('/', async (req, res) => {
+	try {
+		let reports = await Report.findAll({
+			include: [
+				{model: User, as: 'FlaggedByUser' },
+				{ model: Post, include: Post.includeOptions() }
+			]
+		})
+
+		res.json(reports)
+	} catch (e) {
+		console.log(e)
+
+		res.status(500)
+		res.json({
+			errors: [Errors.unknown]
+		})
+	}
+})
+router.delete('/:id', async (req, res) => {
+	try {
+		let report = await Report.findById(req.params.id)
+		if(!report) throw Report.InvalidPostId(req.params.id)
+
+		await report.destroy()
+		res.json({ success: true })
+	} catch (e) {
+		if(e instanceof Sequelize.ValidationError) {
+			res.status(400)
+			res.json(e)
+		} else {
+			console.log(e)
+
+			res.status(500)
+			res.json({
+				errors: [Errors.unknown]
+			})
+		}
+	}
+})
+
+module.exports = router
